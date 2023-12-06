3dc21c4ca5156e8843c4695269361a6948e4de6c
jbahner <julian@bahner.de>
2023-12-06 17:57:18 +0100

    Put Day 1 into object

--- 2023/src/main/scala/jbahner/aoc/Day01.scala
+++ 2023/src/main/scala/jbahner/aoc/Day01.scala
@@ -1,39 +1,40 @@
 package jbahner.aoc
 
 import scala.collection.mutable.LinkedHashMap
+object Day01 {
+  def parseAndSum(arr: Array[String]) = arr
+    .map(line => (line.find(_.isDigit).get, line.findLast(_.isDigit).get))
+    .map((c1, c2) => c1.asDigit * 10 + c2.asDigit)
+    .fold(0)((x, y) => x + y)
 
-def parseAndSum(arr: Array[String]) = arr
-  .map(line => (line.find(_.isDigit).get, line.findLast(_.isDigit).get))
-  .map((c1, c2) => c1.asDigit * 10 + c2.asDigit)
-  .fold(0)((x, y) => x + y)
+  def part1(input: String): Int = parseAndSum(input.split("\n"))
 
-def part1(input: String): Int = parseAndSum(input.split("\n"))
-
-def part2(input: String): Int = {
-  val numbers = LinkedHashMap(
-    "oneight" -> "18",
-    "twone" -> "21",
-    "threeight" -> "38",
-    "fiveight" -> "58",
-    "sevenine" -> "79",
-    "eightwo" -> "82",
-    "eighthree" -> "83",
-    "nineight" -> "98",
-    "one" -> "1",
-    "two" -> "2",
-    "three" -> "3",
-    "four" -> "4",
-    "five" -> "5",
-    "six" -> "6",
-    "seven" -> "7",
-    "eight" -> "8",
-    "nine" -> "9"
-  )
-  parseAndSum(
-    numbers
-      .foldLeft(input)((cur, replacement) =>
-        cur.replaceAll(replacement._1, replacement._2)
-      )
-      .split("\n")
-  )
+  def part2(input: String): Int = {
+    val numbers = LinkedHashMap(
+      "oneight" -> "18",
+      "twone" -> "21",
+      "threeight" -> "38",
+      "fiveight" -> "58",
+      "sevenine" -> "79",
+      "eightwo" -> "82",
+      "eighthree" -> "83",
+      "nineight" -> "98",
+      "one" -> "1",
+      "two" -> "2",
+      "three" -> "3",
+      "four" -> "4",
+      "five" -> "5",
+      "six" -> "6",
+      "seven" -> "7",
+      "eight" -> "8",
+      "nine" -> "9"
+    )
+    parseAndSum(
+      numbers
+        .foldLeft(input)((cur, replacement) =>
+          cur.replaceAll(replacement._1, replacement._2)
+        )
+        .split("\n")
+    )
+  }
 }
--- 2023/src/main/scala/jbahner/aoc/Main.scala
+++ 2023/src/main/scala/jbahner/aoc/Main.scala
@@ -2,8 +2,8 @@ package jbahner.aoc
 
 import scala.io.Source
 @main def runAllDays = {
-  println(s"Day01, Part01: ${part1(loadInput("day01"))}")
-  println(s"Day01, Part02: ${part2(loadInput("day01"))}")
+  println(s"Day01, Part01: ${Day01.part1(loadInput("day01"))}")
+  println(s"Day01, Part02: ${Day01.part2(loadInput("day01"))}")
 }
 
 def loadInput(day: String): String =
