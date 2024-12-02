45ae2bf64acdb070ace63a073065ba553cb1b714
jbahner <julian@bahner.de>
2024-12-02 19:49:16 +0100

    Day 1 Part 2

--- 2024/day01/day01.py
+++ 2024/day01/day01.py
@@ -1,18 +1,39 @@
 from util import *
 import re
 
-list1 = []
-list2 = []
 
-lines = load_input()
-for line in lines:
-    splitted = re.split('\\s+', line.rstrip())
-    list1.append(int(splitted[0]))
-    list2.append(int(splitted[1]))
+def part1(list1, list2):
+    list1 = sorted(list1)
+    list2 = sorted(list2)
+    res = 0
+    for i in range(len(list1)):
+        res += abs(list1[i] - list2[i])
+    print('part 1', res)
 
-res = 0
-list1 = sorted(list1)
-list2 = sorted(list2)
-for i in range(len(list1)):
-    res += abs(list1[i] - list2[i])
-print(res)
+
+def part2(list1, list2):
+    counts = dict()
+
+    for num in list2:
+        counts[num] = counts.get(num, 0) + 1
+    res = 0
+
+    for num in list1:
+        res += num * counts.get(num, 0)
+
+    print('part 2', res)
+
+
+def main():
+    lines = load_input()
+    list1 = []
+    list2 = []
+    for line in lines:
+        splitted = re.split('\\s+', line.rstrip())
+        list1.append(int(splitted[0]))
+        list2.append(int(splitted[1]))
+    part1(list1, list2)
+    part2(list1, list2)
+
+
+main()
--- 2024/util/util.py
+++ 2024/util/util.py
@@ -2,6 +2,6 @@ import inspect
 import os
 
 
-def load_input(test = False):
+def load_input(test=False, part=None):
     path = os.path.dirname((inspect.stack()[1])[1])
-    return [line.rstrip() for line in open(f"{path}/{'test_' if test else ''}input.txt", "r", encoding="utf-8")]
+    return [line.rstrip() for line in open(f"{path}/{'test_' if test else ''}input{'' if part is None else f'_part{part}'}.txt", "r", encoding="utf-8")]
