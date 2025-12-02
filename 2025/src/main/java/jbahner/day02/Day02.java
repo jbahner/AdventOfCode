ee4f230f528b4344ab9822d064d37ef6fb58be0f
jba <julian.bahner@corix.ch>
2025-12-03 00:13:11 +0100

    Day 2 Part 2

--- 2025/src/main/java/jbahner/day02/Day02.java
+++ 2025/src/main/java/jbahner/day02/Day02.java
@@ -2,6 +2,8 @@ package jbahner.day02;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.LongPredicate;
+import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
 import jbahner.AbstractDay;
@@ -9,19 +11,6 @@ import jbahner.SplitParser;
 import jbahner.util.InputReaderException;
 
 public class Day02 extends AbstractDay<List<Day02.IdRange>> {
-
-  public Day02() throws InputReaderException {
-    super(2, new SplitParser(",").mapEach(IdRange::fromInputSlice));
-  }
-
-  static void main() throws Exception {
-    run(Day02.class);
-  }
-
-  @Override
-  public Long part1() {
-    return input.stream().flatMapToLong(IdRange::filterInvalids).sum();
-  }
 
   // Here I assume that all given inputs are actual IDs, so no leading 0 is present
   record IdRange(long lower, long upper) {
@@ -34,19 +23,58 @@ public class Day02 extends AbstractDay<List<Day02.IdRange>> {
       return new IdRange(parts.getFirst(), parts.getLast());
     }
 
-    LongStream filterInvalids() {
+    LongStream filter(LongPredicate hasRepeatingPattern) {
       return LongStream.range(lower, upper + 1)
-              .filter(IdRange::isValid);
+              .filter(hasRepeatingPattern);
     }
+  }
 
-    static boolean isValid(long id) {
-      String str = String.valueOf(id);
-      return str.substring(0, str.length() / 2).equals(str.substring(str.length() / 2));
-    }
+  public Day02() throws InputReaderException {
+    super(2, new SplitParser(",").mapEach(IdRange::fromInputSlice));
+  }
+
+  static void main() throws Exception {
+    run(Day02.class);
+  }
+
+  private Long solve(LongPredicate hasRepeatingPattern) {
+    return input.stream().flatMapToLong(idRange -> idRange.filter(hasRepeatingPattern)).sum();
+  }
+
+  @Override
+  public Long part1() {
+    LongPredicate hasRepeatingPattern = (val) -> {
+      String id = String.valueOf(val);
+      int middle = id.length() / 2;
+      return id.substring(0, middle).equals(id.substring(middle));
+    };
+    return solve(hasRepeatingPattern);
   }
 
   @Override
   public Long part2() {
-    return null;
+    LongPredicate hasRepeatingPattern = (val) -> {
+      String id = String.valueOf(val);
+      int idLength = id.length();
+      // Only patterns up to half the total length are possible
+      for (int i = 1; i <= idLength / 2; i++) {
+        String candidate = id.substring(0, i);
+        int candidateLength = candidate.length();
+        // If the length of the entire ID is not a multiple of the candidate length, we can skip
+        if (idLength % candidateLength != 0) continue;
+        boolean repeatingPattern = splitIntoChunks(id.substring(candidateLength), candidateLength)
+                .stream()
+                .allMatch(candidate::equals);
+        if (repeatingPattern) return true;
+      }
+      return false;
+    };
+    return solve(hasRepeatingPattern);
+  }
+
+  private static List<String> splitIntoChunks(String s, int size) {
+    return IntStream.iterate(0, i -> i < s.length(), i -> i + size)
+            .mapToObj(i -> s.substring(i, Math.min(i + size, s.length())))
+            .toList();
   }
 }
--- 2025/src/test/java/jbahner/day02/Day02Test.java
+++ 2025/src/test/java/jbahner/day02/Day02Test.java
@@ -1,12 +1,10 @@
 package jbahner.day02;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import jbahner.AbstractDayTest;
 
 class Day02Test extends AbstractDayTest<Day02> {
 
   protected Day02Test()  {
-    super(Day02.class, 1227775554L, null);
+    super(Day02.class, 1227775554L, 4174379265L);
   }
 }
